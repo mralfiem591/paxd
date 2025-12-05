eb6b285d3fa513ee121987bdc8e16b18dad8ef3e
mralfiem591 <iamamccabe@gmail.com>
2025-12-05 14:08:44 +0000

    debug: add debug message for firstrun file

--- packages/com.mralfiem591.paxd-gui/src/paxd_gui.py
+++ packages/com.mralfiem591.paxd-gui/src/paxd_gui.py
@@ -957,6 +957,7 @@ class PaxDGUI:
         self.load_packages()
         
         # If a .FIRSTRUN file exists, show welcome message
+        print(f"DEBUG: Checking for .FIRSTRUN file at {os.path.join(os.path.dirname(__file__), '.FIRSTRUN')}")
         if os.path.exists(os.path.join(os.path.dirname(__file__), '.FIRSTRUN')):
             messagebox.showinfo(
                 "Welcome to PaxD GUI!",
